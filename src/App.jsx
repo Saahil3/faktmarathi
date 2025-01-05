66069d8c56c32b1abc791fcb2797baae9890a996
sahil churi <churisahil.3@gmail.com>
2025-01-05 12:35:55 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export default function App() {
   const handleTextTranslate = async () => {
     try {
       const response = await axios.post(
-        "http://faktmarathi.onrender.com/translate-text",
+        "https://faktmarathi.onrender.com/translate-text",
         { text }
       );
       setTranslatedText(response.data.translated_text);
@@ -30,7 +30,7 @@ export default function App() {
 
     try {
       const response = await axios.post(
-        "http://faktmarathi.onrender.com/translate-image",
+        "https://faktmarathi.onrender.com/translate-image",
         formData,
         { headers: { "Content-Type": "multipart/form-data" } }
       );
@@ -47,7 +47,7 @@ export default function App() {
 
     try {
       const response = await axios.post(
-        "http://faktmarathi.onrender.com/translate-document",
+        "https://faktmarathi.onrender.com/translate-document",
         formData,
         { headers: { "Content-Type": "multipart/form-data" } }
       );
@@ -64,7 +64,7 @@ export default function App() {
 
     try {
       const response = await axios.post(
-        "http://faktmarathi.onrender.com/translate-ppt",
+        "https://faktmarathi.onrender.com/translate-ppt",
         formData,
         { headers: { "Content-Type": "multipart/form-data" }, responseType: 'blob' }
       );
